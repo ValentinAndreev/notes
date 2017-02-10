d98d60c997cab864969a43172b2edde3155bcf64
ValentinAndreev <valentinandreev80@gmail.com>
2017-02-10 13:01:32 +0300

    CRUD

--- app/assets/javascripts/tasks.coffee
+++ app/assets/javascripts/tasks.coffee
@@ -1,3 +1,15 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$ ->
+  $('#show-tasks').click ->
+    $('#show-tasks').hide()
+    $('#close-tasks').show()
+    $('.tasks').fadeIn()
+
+  $('#close-tasks').click ->
+    $('#close-tasks').hide()
+    $('#show-tasks').show()
+    $('.tasks').fadeOut()    
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,32 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create!(task_params)
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes!(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.destroy(params[:id])
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:text, :date, :status)
   end
 end
